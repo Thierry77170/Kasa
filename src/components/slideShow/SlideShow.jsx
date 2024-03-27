6957b47bae36a93a7558d25557e2b5f6cf418ff8
Thierry De Sousa <elanion2007@gmail.com>
2024-03-27 12:01:00 +0100

    updated SlideShow component

--- src/components/slideShow/SlideShow.jsx
+++ src/components/slideShow/SlideShow.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import listingsData from '../../datas/logements.json';
 import leftArrow from '../../assets/leftArrow.png';
@@ -7,18 +8,26 @@ import './slideShow.css';
 function SlideShow() {
     const { id } = useParams(); 
     const selectedListing = listingsData.find(listing => listing.identifiant === id ); 
-    const imagesSlide = selectedListing["des photos"];
-    console.log(imagesSlide.length);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    
+    const nextSlide = () => {
+        setCurrentImageIndex((imageIndex) => (imageIndex + 1) % selectedListing["des photos"].length);
+    }
+
+    const prevSlide = () => {
+        setCurrentImageIndex((imageIndex) => (imageIndex - 1 + selectedListing["des photos"].length) % selectedListing["des photos"].length);
+    }
 
     return (
         <div className='slideShowContainer'>
-            <img src={imagesSlide[0]}
+            <img src={selectedListing["des photos"][currentImageIndex]}
                 alt="image de couverture" 
                 className='slideShowContainer__img' /
             >
+            <div className="slideShowContainer__numbertext">{currentImageIndex + 1}/{selectedListing["des photos"].length}</div>
             <div className='slideShowContainer__arrow'>
-                <img src={leftArrow} alt="flèche gauche" className='slideShowContainer__arrow--left' />
-                <img src={rightArrow} alt="flèche droite" className='slideShowContainer__arrow--right' />
+                <img src={leftArrow} alt="flèche gauche" className='slideShowContainer__arrow--left' onClick={prevSlide} />
+                <img src={rightArrow} alt="flèche droite" className='slideShowContainer__arrow--right' onClick={nextSlide} />
             </div>
 
         </div>
--- src/pages/listing/ListingDetails.jsx
+++ src/pages/listing/ListingDetails.jsx
@@ -1,12 +1,8 @@
-import { useParams } from 'react-router-dom';
-import listingsData from '../../datas/logements.json';
+
 import SlideShow from '../../components/slideShow/SlideShow.jsx';
 import './listingDetails.css'
 
 function ListingDetails() {
-    const { id } = useParams(); 
-    const selectedListing = listingsData.find(listing => listing.identifiant === id ); 
-
     return (
         <div className='listingDetails'>
             <SlideShow />
