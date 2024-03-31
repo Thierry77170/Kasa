af47efb39610104b55ac41faf041424a557a8850
Thierry De Sousa <elanion2007@gmail.com>
2024-03-31 01:41:00 +0100

    creating the 'About' view

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,43 @@
 import MaskGroup from '../../components/maskGroup/MaskGroup.jsx'
+import Dropdown from '../../components/dropdown/Dropdown.jsx'
 import './about.css'
 
 function About() {
     return (
         <main className='about'>
             <MaskGroup viewType="about"/>
+            <div className="dropdownContainer">
+                <Dropdown 
+                    className="dropdownContainer__dropdown"
+                    title="Fiabilité" 
+                    text="Les annonces postées sur Kasa garantissent une fiabilité totale. 
+                        Les photos sont conformes aux logements, 
+                        et toutes les informations sont régulièrement vérifiées par nos équipes." 
+                />
+                <Dropdown 
+                    className="dropdownContainer__dropdown"
+                    title="Respect" 
+                    text="La bienveillance fait partie des valeurs fondatrices de Kasa.
+                    Tout comportement discriminatoire ou de perturbation du voisinage 
+                    entraînera une exclusion de notre plateforme." 
+                />
+                <Dropdown 
+                    className="dropdownContainer__dropdown"
+                    title="Service" 
+                    text="La bienveillance fait partie des valeurs fondatrices de Kasa.
+                    Tout comportement discriminatoire ou de perturbation du voisinage 
+                    entraînera une exclusion de notre plateforme." 
+                />
+                <Dropdown 
+                    className="dropdownContainer__dropdown"
+                    title="Sécurité" 
+                    text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs,
+                    chaque logement correspond aux critères de sécurité établis par nos services.
+                    En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier 
+                    que les standars sont bien respectés. 
+                    Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." 
+                />
+            </div>
         </main>
     );
 }
--- src/pages/listing/ListingDetails.jsx
+++ src/pages/listing/ListingDetails.jsx
@@ -1,5 +1,7 @@
 import { useParams } from 'react-router-dom'
 import listingsData from '../../datas/logements.json'
+import redStar from '../../assets/redStar.png'
+import grayStar from '../../assets/grayStar.png'
 import Keywords from '../../components/keywords/Keywords.jsx';
 import SlideShow from '../../components/slideShow/SlideShow.jsx'
 import Dropdown from '../../components/dropdown/Dropdown.jsx'
@@ -8,6 +10,19 @@ import './listingDetails.css'
 function ListingDetails() {
     const { id } = useParams(); 
     const selectedListing = listingsData.find(listing => listing.identifiant === id ); 
+
+    const renderStars = (note) => {
+        const maxStars = 5;
+        const stars = [];
+        for (let i = 1; i <= maxStars; i++) {
+            if (i <= note) {
+                stars.push(<img key={i} src={redStar} alt="étoile rouge" className='noteContainer__stars' />);
+            } else {
+                stars.push(<img key={i} src={grayStar} alt="étoile grise" className='noteContainer__stars' />);
+            }
+        }
+        return stars;
+    };
 
     return (
         <main className='listingDetails'>
@@ -20,13 +35,19 @@ function ListingDetails() {
                         <Keywords />
                     </div>       
                 </div>
-                <div className='hoteContainer'>
-                    <p className='hoteContainer__name'>{selectedListing["hôte"]["nom"]}</p>
-                    <img src={selectedListing["hôte"]["photo"]} 
-                        alt="photo de l'hôte" 
-                        className="hoteContainer__photo" 
-                    />
+                <div className='hoteAndNoteContainer'>
+                    <div className='hoteContainer'>
+                        <p className='hoteContainer__name'>{selectedListing["hôte"]["nom"]}</p>
+                        <img src={selectedListing["hôte"]["photo"]} 
+                            alt="photo de l'hôte" 
+                            className="hoteContainer__photo" 
+                        />
+                    </div>
+                    <div className='noteContainer'>
+                        {renderStars(parseInt(selectedListing.note))}
+                    </div>
                 </div>
+                
             </section>
             <section className='containerDescription'>
                 <Dropdown title="Description" text={selectedListing.description} />
